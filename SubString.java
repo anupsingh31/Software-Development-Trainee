a899143f0f0af534cbbf927fec9933e7ada69d63
Anupam Kumar Singh <48378178+anupsingh31@users.noreply.github.com>
2021-07-03 15:43:15 +0530

    Day 3 Assignment

--- SubString.java
+++ SubString.java
@@ -1,3 +1,13 @@
+/* Write program for following using substring 
+Input:- " https://www.swabhavtech.com@
+java?abc";
+Output:-
+Company name:- swabhav tech
+Lang:- java
+Intern name:- abc( mention your name */
+
+
+
 package com.techlab.assignment.test;
 
 public class SubString {
@@ -5,7 +15,7 @@ public class SubString {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String input="https://www.swabhavtech.com@java?Anupamkumar";
-		System.out.println("Company name : "+input.substring(12,23));
+		System.out.println("Company name : "+input.substring(input.indexOf('.')+1,input.lastIndexOf('.')));
 		System.out.println("Lang : "+input.substring(input.indexOf('@')+1,input.indexOf('?')));
 		System.out.println("Intern name : "+input.substring(input.indexOf('?')+1,input.length()));
 		
