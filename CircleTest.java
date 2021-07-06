499e76f8bcd6c6d8edcaffed4702974cd019cff0
Anupam Kumar Singh <48378178+anupsingh31@users.noreply.github.com>
2021-07-06 21:10:01 +0530

    Update CircleTest.java

--- CircleTest.java
+++ CircleTest.java
@@ -1,3 +1,6 @@
+// Write a program to calculate area and perimeter of circle
+
+
 package com.techlab.demo;
 
 import java.util.Scanner;
@@ -6,19 +9,22 @@ public class CircleTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Enter 1 for Area, 2 for Parameter");
+		System.out.println("Enter the value of radius : ");
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
+		int r = sc.nextInt();
+		/* Normal calculation of area and perimeter of circle 
+		System.out.println("Area of circle is "+3.142*r*r);
+		System.out.println("Perimeter of circle is "+2*3.142*r); */
+		System.out.println("Enter 1 for Area, 2 for Parameter");
+		int a =sc.nextInt();
 		switch (a) {
 		case 1:
 			System.out.println("Enter the radius : ");
-			int r = sc.nextInt();
 			System.out.println(3.142 * r * r);
 			break;
 		case 2:
 			System.out.println("Enter the radius : ");
-			int r1 = sc.nextInt();
-			System.out.println(2 * 3.142 * r1);
+			System.out.println(2 * 3.142 * r);
 			break;
 		default:
 			System.out.println("Enter wrong choice");
