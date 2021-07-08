807cb38bdd71dd02afc50aec272702cda0435741
Anupam Kumar Singh <48378178+anupsingh31@users.noreply.github.com>
2021-07-08 14:58:11 +0530

    Update Circle.java

--- Day 5 Assignment/Circle-App/Circle.java
+++ Day 5 Assignment/Circle-App/Circle.java
@@ -5,7 +5,7 @@ package com.techlab.circle;
 public class Circle {
 	public float radius;
 	public String color;
-	public void diplaydata()
+	public void DisplayData()
 	{
 		System.out.println("radius = "+radius+" color = "+color);
 	}
