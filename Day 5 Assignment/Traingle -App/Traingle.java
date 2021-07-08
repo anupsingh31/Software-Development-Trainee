d4c3019f13e0d33dc7bd7b239e31967b2f0d0308
Anupam Kumar Singh <48378178+anupsingh31@users.noreply.github.com>
2021-07-08 15:02:30 +0530

    Update Traingle.java

--- Day 5 Assignment/Traingle -App/Traingle.java
+++ Day 5 Assignment/Traingle -App/Traingle.java
@@ -5,14 +5,14 @@ package com.techlab.traingle;
 
 public class Traingle {
 	public double side1,side2,side3;
-	public void perimeter()
+	public double Perimeter()
 	{
-		System.out.println("Perimeter of Traingle is : "+(side1+side2+side3));
+		return (side1+side2+side3);
 	}
-	public void area()
+	public double Area()
 	{
 		double s=((double)(side1+side2+side3)/2);
-		System.out.println("perimeter of traingle is : "+Math.sqrt(s*(s-side1)*(s-side2)*(s-side3)));
+		return Math.sqrt(s*(s-side1)*(s-side2)*(s-side3));
 	}
 
 }
