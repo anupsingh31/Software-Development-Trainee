b12613905dae59458a3b7385b0d5269f58a9ccc0
Anupam Kumar Singh <48378178+anupsingh31@users.noreply.github.com>
2021-07-10 11:20:35 +0530

    Add files via upload

--- Day 7 Assignment/Player App/Player.java
+++ Day 7 Assignment/Player App/Player.java
@@ -6,13 +6,14 @@ public class Player {
 	private int id;
 	private String name;
 	private int age;
+	private int ELDER=0,MAXPLAYER=4;
 	
 	public Player(int id, String name, int age) {
 		this.id = id;
 		this.name = name;
 		this.age = age;
 		
-
+		System.out.println("Id "+this.id+" Name "+this.name+" Age "+this.age);
 	}
 
 	public int getId() {
@@ -26,8 +27,15 @@ public class Player {
 	public int getAge() {
 		return age;
 	}
-	
-	public void whoISElder(Player[] eld) {
+	public void whoISElder(Player p)
+	{
+		if(ELDER<p.age)
+		ELDER=p.age;
+		if(MAXPLAYER==p.id)
+		System.out.println("Highest age of player is "+ELDER);
+		
+	}
+/*	public void whoISElder(Player[] eld) {
 		
 		int temp=eld[0].getAge();
 		for(int i=1;i<eld.length;i++)
@@ -38,5 +46,5 @@ public class Player {
 		}
 		System.out.println("elder is " + temp);
 		
-	}
+	}*/
 }
