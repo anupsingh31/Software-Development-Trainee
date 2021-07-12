ede7e6cc1af018a4bc516030a11a13cc1e356d18
Anupam Kumar Singh <48378178+anupsingh31@users.noreply.github.com>
2021-07-12 12:46:31 +0530

    Add files via upload

--- Day 7 Assignment/Player App/PlayerTest.java
+++ Day 7 Assignment/Player App/PlayerTest.java
@@ -7,18 +7,14 @@ public class PlayerTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Player[] p = new Player[4];
-		Player p1 = new Player(1, "Anupam", 20);
+		Player p1 = new Player(1, "Anupam", 21);
 		p[0] = p1;
-		p[0].whoISElder(p1);
-		Player p2 = new Player(2, "Amit", 21);
+		Player p2 = new Player(2, "Amit", 18);
 		p[1] = p2;
-		p[0].whoISElder(p2);
-		Player p3 = new Player(3, "sumit", 20);
+		Player p3 = new Player(3, "sumit", 29);
 		p[2] = p3;
-		p[0].whoISElder(p3);
-		Player p4 = new Player(4, "ankit", 22);
+		Player p4 = new Player(4, "ankit", 40);
 		p[3] = p4;
-		p[0].whoISElder(p4);
 		printPlayerInfo(p);
 	}
 	public static void printPlayerInfo(Player[] info) {
@@ -26,6 +22,8 @@ public class PlayerTest {
 			System.out.println("ID of " + (i + 1) + " Player is : " + info[i].getId());
 			System.out.println("Name of " + (i + 1) + " Player is : " + info[i].getName());
 			System.out.println("Age of " + (i + 1) + " Player is : " + info[i].getAge());
+			info[0]=info[0].whoISElder(info[i]);
+			System.out.println("Highest Age of Player is "+info[i].getAge());
 		}
 	}
 }
