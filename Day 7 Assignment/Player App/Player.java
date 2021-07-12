9d084b94b5e24846901dafb6d390d59c23ff29e5
Anupam Kumar Singh <48378178+anupsingh31@users.noreply.github.com>
2021-07-12 20:59:29 +0530

    Add files via upload

--- Day 7 Assignment/Player App/Player.java
+++ Day 7 Assignment/Player App/Player.java
@@ -1,19 +1,16 @@
 package com.techlab.player;
 
-
 public class Player {
 
 	private int id;
 	private String name;
 	private int age;
-	//private int ELDER=0,MAXPLAYER=4;
-	
+	// private int ELDER=0,MAXPLAYER=4;
+
 	public Player(int id, String name, int age) {
 		this.id = id;
 		this.name = name;
 		this.age = age;
-		
-		System.out.println("Id "+this.id+" Name "+this.name+" Age "+this.age);
 	}
 
 	public int getId() {
@@ -27,30 +24,24 @@ public class Player {
 	public int getAge() {
 		return age;
 	}
-	public Player whoISElder(Player p)
-	{
-		
-		if(age<p.age)
-		return p;
-		else
-		{
-			p.age=age;
-			p.name=name;
-			p.id=id;
+
+	public Player whoISElder(Player p) {
+
+		if (this.age < p.age)
 			return p;
+		else {
+			
+			return this;
 		}
-		
+
 	}
-/*	public void whoISElder(Player[] eld) {
-		
-		int temp=eld[0].getAge();
-		for(int i=1;i<eld.length;i++)
-		{
-			//System.out.println(eld[i]);
-			if(temp<eld[i].getAge())
-				temp=eld[i].getAge();
-		}
-		System.out.println("elder is " + temp);
-		
-	}*/
+	/*
+	 * public void whoISElder(Player[] eld) {
+	 * 
+	 * int temp=eld[0].getAge(); for(int i=1;i<eld.length;i++) {
+	 * //System.out.println(eld[i]); if(temp<eld[i].getAge()) temp=eld[i].getAge();
+	 * } System.out.println("elder is " + temp);
+	 * 
+	 * }
+	 */
 }
