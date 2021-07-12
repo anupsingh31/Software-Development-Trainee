fc4c8ab48a4d1fcec0c58500a07b2586d08447c8
Anupam Kumar Singh <48378178+anupsingh31@users.noreply.github.com>
2021-07-12 21:02:36 +0530

    Add files via upload

--- Day 7 Assignment/Candidate App/CandidateTest.java
+++ Day 7 Assignment/Candidate App/CandidateTest.java
@@ -8,24 +8,21 @@ public class CandidateTest {
 		// TODO Auto-generated method stub
 		Candidate[] c = new Candidate[4];
 
-		Candidate c1 = new Candidate(1, "Anupam", 20, "45000");
-		c[0] = c1;
-		Candidate c2 = new Candidate(2, "Amit", 21, "4000");
-		c[1] = c2;
-		Candidate c3 = new Candidate(3, "sumit", 20, "50000");
-		c[2] = c3;
-		Candidate c4 = new Candidate(4, "ankit", 22, "35000");
-		c[3] = c4;
+		c[0] = new Candidate(1, "Anupam", 20, "45000");
+		c[1] = new Candidate(2, "Amit", 21, "4000");
+		c[2] = new Candidate(3, "sumit", 20, "50000");
+		c[3] = new Candidate(4, "ankit", 22, "35000");
+		Candidate temp=new Candidate(0,"",0,"");
 
 		for (int i = 0; i < c.length; i++) {
 			System.out.println("ID of " + (i + 1) + " Employee is : " + c[i].getId());
 			System.out.println("Name of " + (i + 1) + " Employee is : " + c[i].getName());
 			System.out.println("CreditPoint of " + (i + 1) + " Employee is : " + c[i].getCreditPoint());
-			c1 = c1.whoISBetter(c[i]);
+			//temp = temp.whoISBetter(c[i]);
 
 		}
-		System.out.println("Best Candidate is  whose id = " + c1.getId() + " Name = " + c1.getName() + " CreditPoint = "
-				+ c1.getCreditPoint());
+		System.out.println("Best Candidate is  whose id = " + temp.getId() + " Name = " + temp.getName() + " CreditPoint = "
+				+ temp.getCreditPoint());
 
 	}
 
