5f0f267ea74389ff1d7971d4945fd073041c99cf
Anupam Kumar Singh <48378178+anupsingh31@users.noreply.github.com>
2021-07-12 20:58:53 +0530

    Add files via upload

--- Day 7 Assignment/Player App/PlayerTest.java
+++ Day 7 Assignment/Player App/PlayerTest.java
@@ -7,23 +7,29 @@ public class PlayerTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Player[] p = new Player[4];
-		Player p1 = new Player(1, "Anupam", 21);
-		p[0] = p1;
-		Player p2 = new Player(2, "Amit", 18);
-		p[1] = p2;
-		Player p3 = new Player(3, "sumit", 29);
-		p[2] = p3;
-		Player p4 = new Player(4, "ankit", 40);
-		p[3] = p4;
-		printPlayerInfo(p);
+		p[0] = new Player(1, "Anupam", 21);
+
+		 p[1] = new Player(2, "Amit", 18);
+		 p[2] = new Player(3, "sumit", 29);
+		 p[3] = new Player(4, "ankit", 40);
+		 Player p1=new Player(0, "", 0);
+		 for (int i = 0; i < p.length; i++) {
+				System.out.println("ID of " + (i + 1) + " Player is : " + p[i].getId());
+				System.out.println("Name of " + (i + 1) + " Player is : " + p[i].getName());
+				System.out.println("Age of " + (i + 1) + " Player is : " + p[i].getAge());
+				p1 = p1.whoISElder(p[i]);
+				System.out.println("Highest Age of Player is " + p1.getAge());
+			}
+		
 	}
+
 	public static void printPlayerInfo(Player[] info) {
 		for (int i = 0; i < info.length; i++) {
 			System.out.println("ID of " + (i + 1) + " Player is : " + info[i].getId());
 			System.out.println("Name of " + (i + 1) + " Player is : " + info[i].getName());
 			System.out.println("Age of " + (i + 1) + " Player is : " + info[i].getAge());
-			info[0]=info[0].whoISElder(info[i]);
-			System.out.println("Highest Age of Player is "+info[i].getAge());
+			info[0] = info[0].whoISElder(info[i]);
+			System.out.println("Highest Age of Player is " + info[i].getAge());
 		}
 	}
 }
