7e22ccb1e7f4a760f7b3ab836e60be321896d325
Anupam Kumar Singh <48378178+anupsingh31@users.noreply.github.com>
2021-07-13 17:34:17 +0530

    Add files via upload

---
+++ Day 8 Assignment/engineering-app/src/com/techlab/engineering/college/Professor.java
@@ -0,0 +1,16 @@
+package com.techlab.engineering.college;
+
+public class Professor extends Person {
+	private double salary;
+
+	public Professor(int id, String name, String dob, double salary) {
+		super(id, name, dob);
+		this.salary = salary;
+
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+}
---
+++ Day 8 Assignment/single-inheritance-app/src/com/techlab/single/inheritance/test/SingleInheritanceTest.java
@@ -0,0 +1,28 @@
+package com.techlab.single.inheritance.test;
+
+
+import com.techlab.single.inheritance.Employee;
+import com.techlab.single.inheritance.Programmer;
+
+public class SingleInheritanceTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Programmer p=new Programmer(1,"anupam",35000,1500);
+		System.out.println("id = "+p.getId()+" name = "+p.getName()+" Salary = "+p.getSalary()+" Bonus = "+p.getBonus());
+		
+/*	// UPCASTING
+		Programmer p1=new Programmer(2,"rohit",40000,2000);
+		Employee e=p1;
+		//System.out.println(p1.getBonus());
+		System.out.println("id = "+e.getId()+" name = "+e.getName()+" Salary = "+e.getSalary()+" bonus = "+p1.getBonus());
+
+		
+		// DOWNCASTING
+		Employee e1=new Employee(2,"rohit",40000);
+		Programmer p2= (Programmer)e1;
+		//System.out.println(p1.getBonus());
+		System.out.println("id = "+p2.getId()+" name = "+p2.getName()+" Salary = "+p2.getSalary()+" bonus = "+p2.getBonus());
+	*/}
+
+}
