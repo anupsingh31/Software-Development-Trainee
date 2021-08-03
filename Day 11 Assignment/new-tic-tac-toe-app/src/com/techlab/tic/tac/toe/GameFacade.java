54792cb4702943b2923ba24a03c0e582825bcdaf
Anupam Kumar Singh <48378178+anupsingh31@users.noreply.github.com>
2021-08-03 12:51:14 +0530

    facade design pattern on tic tac toe app

---
+++ Day 11 Assignment/new-tic-tac-toe-app/src/com/techlab/tic/tac/toe/GameFacade.java
@@ -0,0 +1,67 @@
+package com.techlab.tic.tac.toe;
+
+import java.util.ArrayList;
+
+public class GameFacade {
+
+	private Board board;
+	private ResultAnalayzer analyze;
+	private Player player;
+	private ArrayList<Player> playerData;
+	private Result status = Result.INPROGRESS;
+	private int numFilledBoardData = 0;
+	private Game game;
+	private int positionofBoard;
+
+	public GameFacade(int size) {
+		board = new Board(size);
+		analyze = new ResultAnalayzer(board);
+		playerData = new ArrayList<Player>();
+		game = new Game(board, analyze);
+	}
+
+	public Result getStatus() {
+		return this.status;
+	}
+
+	public int getnumFilledBoardData() {
+		return this.numFilledBoardData;
+	}
+
+	public boolean setnumFilledBoardData(int count) {
+		this.numFilledBoardData = count;
+		game.Play(count, this.positionofBoard);
+		if (analyze.getMoves() + 1 == count) {
+			this.numFilledBoardData = analyze.getMoves();
+			return true;
+		} else {
+			this.status = game.getStatus();
+			if (this.status != Result.INPROGRESS)
+				this.numFilledBoardData += -1;
+			return false;
+		}
+	}
+
+	public ArrayList<Player> getPlayerListData() {
+		return this.playerData;
+	}
+
+	public void setPlayerData(String playerName, Mark mark) {
+		playerData.add(new Player(playerName, mark));
+		game.setPlayer(playerData);
+	}
+
+	public String getPlayerName() {
+		if (numFilledBoardData % 2 == 0) {
+			player = playerData.get(0);
+		} else {
+			player = playerData.get(1);
+		}
+		return player.getPlayerName();
+	}
+
+	public void setPositionOfBoard(int positionofBoard) {
+		this.positionofBoard = positionofBoard;
+	}
+
+}
--- Day 11 Assignment/new-tic-tac-toe-app/src/com/techlab/tic/tac/toe/test/NewTicTacToeTest.java
+++ Day 11 Assignment/new-tic-tac-toe-app/src/com/techlab/tic/tac/toe/test/NewTicTacToeTest.java
@@ -1,28 +1,28 @@
 package com.techlab.tic.tac.toe.test;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.techlab.tic.tac.toe.Board;
 import com.techlab.tic.tac.toe.CustomExceptionHandling;
-import com.techlab.tic.tac.toe.Game;
+import com.techlab.tic.tac.toe.GameFacade;
 import com.techlab.tic.tac.toe.Mark;
-import com.techlab.tic.tac.toe.Player;
 import com.techlab.tic.tac.toe.Result;
-import com.techlab.tic.tac.toe.ResultAnalayzer;
 
 public class NewTicTacToeTest {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		CustomExceptionHandling cs = new CustomExceptionHandling();
+		GameFacade facade;
 		int size;
 		while (true) {
 			System.out.println("Enter the size of Board ");
 			try {
 				size = sc.nextInt();
 				cs.board(size);
+				facade = new GameFacade(size);
 				break;
+
 			} catch (Exception e) {
 				sc.nextLine();
 				System.out.println(e);
@@ -30,68 +30,47 @@ public class NewTicTacToeTest {
 			}
 		}
 
-		Board board = new Board(size);
-
-		ResultAnalayzer analyze = new ResultAnalayzer(board);
-
-		ArrayList<Player> playerData = new ArrayList<Player>();
-		Player player;
-		Result status = Result.INPROGRESS;
-
-		Game game = new Game(board, analyze);
-
 		System.out.println("Intial stage output");
+		printBoard(size);
 
-		printBoard(board.getSize());
-
-		int count = 0;
-
-		while (status.equals(Result.INPROGRESS)) {
-			if (playerData.isEmpty()) {
+		while (facade.getStatus().equals(Result.INPROGRESS)) {
+			if (facade.getPlayerListData().isEmpty()) {
 				System.out.println("Enter the Player1 Name : ");
-				playerData.add(new Player(sc.next(), Mark.X));
-			} else if (playerData.size() == 1 && count % 2 != 0) {
+				facade.setPlayerData(sc.next(), Mark.X);
+			} else if (facade.getPlayerListData().size() == 1 && facade.getnumFilledBoardData() % 2 != 0) {
 				System.out.println("Enter the Player2 Name : ");
-				playerData.add(new Player(sc.next(), Mark.O));
+				facade.setPlayerData(sc.next(), Mark.O);
 			}
 
 			System.out.print("Enter Position : ");
-			if (count % 2 == 0) {
-				player = playerData.get(0);
-				System.out.println(player.getPlayerName());
+			if (facade.getnumFilledBoardData() % 2 == 0) {
+				System.out.println(facade.getPlayerName());
 			} else {
-				player = playerData.get(1);
-				System.out.println(player.getPlayerName());
+				System.out.println(facade.getPlayerName());
 			}
 
 			int positionofBoard;
 			try {
 				positionofBoard = sc.nextInt();
-				cs.validate(positionofBoard, board.getSize());
+				cs.validate(positionofBoard, size);
+				facade.setPositionOfBoard(positionofBoard);
 			} catch (Exception e) {
 				System.err.println(e);
 				sc.nextLine();
 				continue;
 			}
-			game.setPlayer(playerData);
-			count++;
-			game.Play(count, positionofBoard);
 
-			if (count == analyze.getMoves() + 1) {
-				count = analyze.getMoves();
+			boolean slotoccupied = facade.setnumFilledBoardData(facade.getnumFilledBoardData() + 1);
+
+			if (slotoccupied)
 				continue;
-			}
-			printBoard(board.getSize());
-			status = game.getStatus();
+
+			printBoard(size);
 
 		}
-		if (count % 2 == 0) {
-			player = playerData.get(1);
-			desicion(status, count, player.getPlayerName());
-		} else {
-			player = playerData.get(0);
-			desicion(status, count, player.getPlayerName());
-		}
+
+		desicion(facade.getStatus(), facade.getPlayerName());
+
 		sc.close();
 
 	}
@@ -105,7 +84,7 @@ public class NewTicTacToeTest {
 		}
 	}
 
-	public static void desicion(Result winner, int count, String player) {
+	public static void desicion(Result winner, String player) {
 		if (winner.equals(Result.DRAW))
 			System.out.println("It's a draw! Thanks for playing.");
 		else
